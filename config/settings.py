1c187212669e1b2cb7a0eb65166ae244519fb9a2
Your Name <your@email.com>
2026-06-23 12:16:21 -0700

    Add home.html template and update settings for template directory

--- config/settings.py
+++ config/settings.py
@@ -62,10 +62,11 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],  # Ee line add chey
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
