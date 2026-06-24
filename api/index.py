b8bfc7cf2395c75e0d1c67e6878a604ee9e4f84b
Shafeeka <shafeeka73820@gmail.com>
2026-06-24 03:31:12 -0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -8,4 +8,4 @@ sys.path.append(str(BASE_DIR))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+app = get_wsgi_application()
