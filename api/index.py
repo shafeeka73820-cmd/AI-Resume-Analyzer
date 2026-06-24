e6c88fc496de18969d7ada25d46301a8a7e514c9
Shafeeka <shafeeka73820@gmail.com>
2026-06-24 01:27:53 -0700

    Initialize WSGI application for Django project

---
+++ api/index.py
@@ -0,0 +1,11 @@
+import os
+import sys
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+sys.path.append(str(BASE_DIR))
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+from django.core.wsgi import get_wsgi_application
+app = get_wsgi_application()
